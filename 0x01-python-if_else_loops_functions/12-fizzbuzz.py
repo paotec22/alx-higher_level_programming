26f8265d20d2a2444466a00104af25a69010a2b8
paotec22 <paotec22@gmail.com>
2023-11-29 08:11:55 -0800

    fizzbuzz number replacement function

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for num in range(1, 101):
+        if num % 3 == 0:
+            print("Fizz", end='')
+        if num % 5 == 0:
+            print("Buzz", end='')
+        if num % 3 and num % 5:
+            print("{:d}".format(num, end=''))
+        print(end=' ')
