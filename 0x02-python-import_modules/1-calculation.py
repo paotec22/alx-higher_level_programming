c7e41f22b1e74386ade869bc4fff1cb377cac3b8
paotec22 <paotec22@gmail.com>
2023-11-30 10:27:03 -0800

    program that imports functions from the file calculator_1.py, does some Maths, and prints the result

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, add(a, b)))
