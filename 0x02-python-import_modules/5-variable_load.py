0487176c222a5633c68cb27331c8a2b9264802c9
paotec22 <paotec22@gmail.com>
2023-12-05 11:45:43 -0800

    program that imports the variable a from the file variable_load_5.py and prints its value

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-from variable_load_5 import a
-
-def assign_var():
-    print("{}".format(a))
-
 if __name__ == "__main__":
-    assign_var()
+    """Print the value of a from variable_load_5"""
+    from variable_load_5 import a
+
+    print(a)
